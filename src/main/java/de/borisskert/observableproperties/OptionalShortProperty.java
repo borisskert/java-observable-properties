5be4a685215106814dc9ef3eb0f8036b5790d680
Boris Skert <boris.skert@gmail.com>
2020-04-05 08:31:23 +0200

    Deploy artifact to sonatype nexus

---
+++ src/main/java/de/borisskert/observableproperties/OptionalShortProperty.java
@@ -0,0 +1,24 @@
+package de.borisskert.observableproperties;
+
+/**
+ * Implements a {@link OptionalProperty} which holds a {@link Short} value.
+ * The value is mandatory and may be null.
+ */
+public class OptionalShortProperty extends SimpleOptionalProperty<Short> {
+
+    /**
+     * Creates a {@link OptionalShortProperty} without an initial value.
+     */
+    public OptionalShortProperty() {
+        super();
+    }
+
+    /**
+     * Creates a {@link OptionalShortProperty} containing an initial value.
+     *
+     * @param value the initial {@link Short} value which may be null.
+     */
+    public OptionalShortProperty(Short value) {
+        super(value);
+    }
+}
