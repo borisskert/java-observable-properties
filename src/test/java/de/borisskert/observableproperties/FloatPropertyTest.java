59cb6f0f3ccbc661b473c5afafe1c55c12a6985a
Boris Skert <boris.skert@gmail.com>
2020-04-05 08:31:23 +0200

    Deploy artifact to sonatype nexus

---
+++ src/test/java/de/borisskert/observableproperties/FloatPropertyTest.java
@@ -0,0 +1,34 @@
+package de.borisskert.observableproperties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class FloatPropertyTest {
+
+    private Property<Float> oneDotTwoProperty;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        oneDotTwoProperty = new FloatProperty(1.2f);
+    }
+
+    @Test
+    public void shouldProvideValue() throws Exception {
+        assertThat(oneDotTwoProperty.get(), is(equalTo(1.2f)));
+    }
+
+    @Test
+    public void shouldNotAllowChangingValueToNull() throws Exception {
+        try {
+            oneDotTwoProperty.set(null);
+            fail("Should throw NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage(), is(equalTo("Parameter 'value' must not be null")));
+        }
+    }
+}
---
+++ src/test/java/de/borisskert/observableproperties/StringPropertyTest.java
@@ -0,0 +1,44 @@
+package de.borisskert.observableproperties;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.fail;
+
+class StringPropertyTest {
+
+    private Property<String> abcProperty;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        abcProperty = new StringProperty("abc");
+    }
+
+    @Test
+    public void shouldProvideValue() throws Exception {
+        assertThat(abcProperty.get(), is(equalTo("abc")));
+    }
+
+    @Test
+    public void shouldNotAllowChangingValueToNull() throws Exception {
+        try {
+            abcProperty.set(null);
+            fail("Should throw NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage(), is(equalTo("Parameter 'value' must not be null")));
+        }
+    }
+
+    @Test
+    public void shouldNotAllowInstanceCreationWithNull() throws Exception {
+        try {
+            new StringProperty(null);
+            fail("Should throw NullPointerException");
+        } catch (NullPointerException e) {
+            assertThat(e.getMessage(), is(equalTo("Parameter 'value' must not be null")));
+        }
+    }
+}
