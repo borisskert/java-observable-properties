708539cd1a6e737fe52aae5771935bfa08052064
Boris Skert <boris.skert@gmail.com>
2020-04-05 08:31:23 +0200

    Deploy artifact to sonatype nexus

---
+++ src/main/java/de/borisskert/observableproperties/StringProperty.java
@@ -0,0 +1,17 @@
+package de.borisskert.observableproperties;
+
+/**
+ * Implements a {@link Property} which holds a {@link String} value.
+ * The value cannot be null.
+ */
+public class StringProperty extends SimpleObjectProperty<String> {
+
+    /**
+     * Creates a {@link StringProperty} containing an initial value.
+     *
+     * @param value the initial {@link String} value.
+     */
+    public StringProperty(String value) {
+        super(value);
+    }
+}
