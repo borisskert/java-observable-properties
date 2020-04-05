2b572472232f0924e6f4229f3963ba2dd277815a
Boris Skert <boris.skert@gmail.com>
2020-04-05 08:31:23 +0200

    Deploy artifact to sonatype nexus

---
+++ src/main/java/de/borisskert/observableproperties/OptionalProperty.java
@@ -0,0 +1,18 @@
+package de.borisskert.observableproperties;
+
+import java.util.Optional;
+
+/**
+ * Represents an optional {@link Property}
+ *
+ * @param <T> the value type
+ */
+public interface OptionalProperty<T> extends Property<T> {
+
+    /**
+     * Provides an {@link Optional} which contains the current value.
+     *
+     * @return the current value as {@link Optional}
+     */
+    Optional<T> asOptional();
+}
