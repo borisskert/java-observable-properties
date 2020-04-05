c17d967bc32dd30eb5e0f37fcfb11a6f9d4c0308
Boris Skert <boris.skert@gmail.com>
2020-04-05 08:31:23 +0200

    Deploy artifact to sonatype nexus

---
+++ src/main/java/de/borisskert/observableproperties/OptionalBooleanProperty.java
@@ -0,0 +1,24 @@
+package de.borisskert.observableproperties;
+
+/**
+ * Implements a {@link OptionalProperty} which holds a {@link Boolean} value.
+ * The value is mandatory and may be null.
+ */
+public class OptionalBooleanProperty extends SimpleOptionalProperty<Boolean> {
+
+    /**
+     * Creates a {@link OptionalBooleanProperty} without an initial value.
+     */
+    public OptionalBooleanProperty() {
+        super();
+    }
+
+    /**
+     * Creates a {@link OptionalBooleanProperty} containing an initial value.
+     *
+     * @param value the initial {@link Boolean} value which may be null.
+     */
+    public OptionalBooleanProperty(Boolean value) {
+        super(value);
+    }
+}
---
+++ src/main/java/de/borisskert/observableproperties/OptionalFloatProperty.java
@@ -0,0 +1,24 @@
+package de.borisskert.observableproperties;
+
+/**
+ * Implements a {@link OptionalProperty} which holds a {@link Float} value.
+ * The value is mandatory and may be null.
+ */
+public class OptionalFloatProperty extends SimpleOptionalProperty<Float> {
+
+    /**
+     * Creates a {@link OptionalFloatProperty} without an initial value.
+     */
+    public OptionalFloatProperty() {
+        super();
+    }
+
+    /**
+     * Creates a {@link OptionalFloatProperty} containing an initial value.
+     *
+     * @param value the initial {@link Float} value which may be null.
+     */
+    public OptionalFloatProperty(Float value) {
+        super(value);
+    }
+}
